fd084f3a09d1de7f7cb3557abe21d6745d105632
DiegoT <mando2567@gmail.com>
2023-05-17 17:44:49 -0500

    FIX template concesionarios

--- projectdjangoapp/empresast/views.py
+++ projectdjangoapp/empresast/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Operador, Vehiculo
@@ -7,13 +7,14 @@ from .models import Operador, Vehiculo
 
 def index(request):
     concesionario_list = Operador.objects.all()
-    return render(request, "empresast/index.html", {
+    print(concesionario_list)
+    return render(request, "empresast/index.html",{
         "concesionario_list": concesionario_list
     })
 
 
 def detalle(request, operador_id):
-    return HttpResponse(f"estas en el detalle del concesionario {operador_id}")
+    return HttpResponse(f"estas en la relación de vehículos de cada concesionario {operador_id}")
 
 
 def vehiculos(request, operador_id):
