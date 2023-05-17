f644cd498b4204b42cd2f489be9211b19abc95ac
DiegoT <mando2567@gmail.com>
2023-05-16 20:00:10 -0500

    Creating of the ORM-Model project

--- projectdjangoapp/empresast/models.py
+++ projectdjangoapp/empresast/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Operador(models.Model):
+    concesionario_nombre = models.CharField(max_length=50)
+    concesionario_codigo = models.IntegerField(default=0)
+  
+    def __str__(self):    
+        concesionario_nombre = self.concesionario_nombre
+        return concesionario_nombre
+    
+class Vehiculo(models.Model):
+    concesionario = models.ForeignKey(Operador, on_delete=models.CASCADE)
+    placa = models.CharField(max_length=7)
+    fecha_vinculacion = models.DateField("Fecha vinculación")
+    modelo = models.IntegerField(default=0)
+    tipologia = models.CharField(max_length=20)
+
+    def __str__(self):
+        placa = self.placa
+        return placa
+    
+
+
+
+  
