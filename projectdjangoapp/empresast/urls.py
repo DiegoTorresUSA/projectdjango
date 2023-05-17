cf4ec099f1de8ddf4b6ce7bfe9c70d85575fbd84
DiegoT <mando2567@gmail.com>
2023-05-17 15:00:07 -0500

    templates created, urls and views adjusted for them

--- projectdjangoapp/empresast/urls.py
+++ projectdjangoapp/empresast/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:operador_id>/", views.detalle, name="Operador"),
+    path("<int:operador_id>/vehiculos", views.vehiculos, name="Vehiculos"),
     ]
--- projectdjangoapp/empresast/views.py
+++ projectdjangoapp/empresast/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Operador, Vehiculo
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Página Principal")
+    concesionario_list = Operador.objects.all()
+    return render(request, "empresast/index.html", {
+        "concesionario_list": concesionario_list
+    })
+
+
+def detalle(request, operador_id):
+    return HttpResponse(f"estas en el detalle del concesionario {operador_id}")
+
+
+def vehiculos(request, operador_id):
+    return HttpResponse(f"estas en la relación de vehículos de cada concesionario {operador_id}")
